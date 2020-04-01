756082ec910fb501032c21feadb593e2e5c7701d
gilweiss <gilweiss90@gmail.com>
2020-04-02 01:50:44 +0300

    added placeholder for requestbox, added button to view request pool, button css cosmetics

--- src/client/index.js
+++ src/client/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import MyForm from  './MyForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
 
 ReactDOM.render(<MyForm />, document.getElementById('root'));
